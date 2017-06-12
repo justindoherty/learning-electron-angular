7e9463affab126afd9a1611fa2634e17a71485c0
Justin Doherty <justin.doherty@solarwinds.com>
2017-06-12 11:53:56 -0400

    List dirs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as fs from 'fs';
+import * as path from 'path';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  data: string[];
+  events: {[key:string]: string} = {};
+
+  ngOnInit() {
+    this.readdir();
+    fs.watch(this.watchedDir, (event, filename) => {
+      console.log(`${filename}: ${event}`);
+      this.events[filename] = event;
+      this.readdir();
+    });
+  }
+
+  private readdir() {
+    fs.readdir(this.watchedDir, (err, data) => {
+      console.log(data);
+      this.data = data;
+    });
+  }
+
+  private get watchedDir() {
+    return path.join(process.env.HOME, 'tmp', 'watched');
+  }
 }
